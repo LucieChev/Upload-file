2a1f17b07f7737998672c7410d352daca79f6306
LucieChev <123562385+LucieChev@users.noreply.github.com>
2023-06-19 11:22:15 +0200

    express 09 upload file

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const multer = require("multer");
+const fs = require("node:fs");
+const { v4: uuidv4 } = require("uuid");
 
 const router = express.Router();
+
+const upload = multer({ dest: "./public/uploads/" });
+
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+  const { originalname } = req.file;
+  const { filename } = req.file;
+  fs.rename(
+    `./public/uploads/${filename}`,
+    `./public/uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+      res.send("File uploaded");
+    }
+  );
+});
 
 const itemControllers = require("./controllers/itemControllers");
 
